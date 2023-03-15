2dd91e6a63e13a12828ac2cac3a08c937c56e486
Okky Maheswara <okkymahes@gmail.com>
2023-03-15 15:38:41 +0800

    owalahhh

--- components/CTA.js
+++ components/CTA.js
@@ -7,7 +7,7 @@ export default function CTA() {
       <div className='text-center'>
         <h2 className='mb-2 text-2xl font-bold capitalize md:text-3xl xl:text-4xl xl:mb-5'>Contact with me</h2>
         <p className='px-3 mt-0 mb-2 md:mx-20 md:mb-5 xl:mt-2 md:text-lg xl:text-2xl xl:mx-56'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, accusantium voluptatem. Sunt debitis adipisci similique sed dolor? Sapiente, nemo enim?</p>
-        <span className='font-bold md:text-lg xl:text-2xl'>Message me today at <Link href='#' className='underline text-primary'>nama-email@domain.com</Link></span>
+        <span className='font-bold md:text-lg xl:text-2xl'>Message me today at <Link href='mailto:nama-email@domain.com' className='underline text-primary'>nama-email@domain.com</Link></span>
       </div>
     </section>
   )
--- components/Project.js
+++ components/Project.js
@@ -5,9 +5,9 @@ export default function Project() {
   return (
     <section className='flex items-center justify-center py-10 px-11 bg-eighth'>
       <div className='text-center'>
-        <h1 className='mb-4 text-2xl font-bold text-white capitalize md:text-4xl'>Projects</h1>
-        <p className='mb-6 text-white'>sadasd</p>
-        <Button href='#' text='View More' className={`text-xs`} />
+        <h1 className='mb-4 text-2xl font-bold text-white capitalize md:text-4xl md:mb-6'>Projects</h1>
+        <p className='mb-6 text-white md:text-xl md:mb-9'>sadasd</p>
+        <Button href='#' text='View More' className={`text-xs md:text-base`} />
       </div>
     </section>
   )
